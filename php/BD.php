e1016661d11d72cdd85cf35cf9e43a77b8df6419
Mescruu <mescruu@gmail.com>
2020-10-12 22:02:13 +0200

    Database handler. Login, Register, Hashing, Encrypt, Decrypt, Deleteing, Partition generating and Receiving data from the database functions

--- php/BD.php
+++ php/BD.php
@@ -1,59 +1,62 @@
 <?php
 
 include_once('user.php');
-include_once("config.php");
+include_once("config.php"); // configuration constants;
 
 class BD {
 
-    public $mysqli;
-    public function __construct($serwer,$user,$pass,$baza) { //utworzenie bazy danych
-        $this->mysqli = new mysqli($serwer,$user,$pass,$baza);
+    public $mysqli; // handle to the database
+
+    public function __construct($server,$user,$pass,$base) {// create a handle to the database
+        $this->mysqli = new mysqli($server,$user,$pass,$base);
 
         if($this->mysqli->connect_errno){
-            printf("Nie udalo sie polaczenie  z serwerem: %s\n",$this->mysqli->connect_error);
+            printf("Connection to the server failed: %s\n",$this->mysqli->connect_error);// creation error
             exit();
         }
-        if($this->mysqli->set_charset("utf8")){}
+        if($this->mysqli->set_charset("utf8")){} // set the character set.
     }
 
-    function __destruct() {
+    function __destruct() { // delete the object
         $this->mysqli->close();
     }
 
+// Function that creates a user object on the basis of data from the database / Argument: user login
     public function getUser($login)
     {
+        // Query the record in the table "user" with the given login
         $sql= "select * from user where login='$login'";
 
+        // execute the query
         $result = $this->mysqli->query($sql);
 
+        // if there is such a record
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
-                $user = new user($row["id"],  $row["login"], $row["password_hash"], $row["salt"], $row["isPasswordKeptAsHash"]);
+                $user = new user($row["id"],  $row["login"], $row["password_hash"], $row["salt"], $row["isPasswordKeptAsHash"]); // create a user object with the database fetched data.
             }
-            return $user;
-
+            return $user; // return the user object
         } else {
-            return null;
+            return null;// return null if the user with the given login does not exist.
         }
     }
+
     public  function  createPartition($login, $password, $description, $web_address, $user_Id){
+        // encrypt the password using the encryption method in the config file and the user's password. 1 means OPENSSL_ZERO_PADDING
+        $encryptedPassword= @openssl_encrypt($password, constant('cypherMethod'), $_SESSION['password'], 0);
 
-        $key = $_SESSION['password'];
-        //$ivlen = openssl_cipher_iv_length(constant('cypherMethod')); //Pobiera długość wektora inicjalizacji szyfru (iv).
-       // $iv = openssl_random_pseudo_bytes($ivlen); //Generuje ciąg pseudolosowych bajtów, z liczbą bajtów określoną przez lengthparametr.
-
-        $encryptedPassword= @openssl_encrypt($password, constant('cypherMethod'), $key, $options=0);
-
-        //wywołanie funkcji wprowadzającej do bazy
+        // call the insert function to the database
         $insert_sql = 'INSERT INTO `password` (`id`, `password`, `id_user`, `web_address`, `description`,`login`) VALUES (NULL, "'.$encryptedPassword.'", "'.$user_Id.'", "'.$web_address.'", "'.$description.'", "'.$login.'");';
         $this->insert($insert_sql);
 
+        //feedback
         return "Position added";
     }
 
+// Get the appropriate partition. argument: the user id
     public function getPartition($userId){
-
+    // SQL query for tabs with passwords of the given user
         $sql= "select * from password where id_user='$userId;'";
 
         $result = $this->mysqli->query($sql);
@@ -63,82 +66,60 @@ class BD {
             while($row = $result->fetch_assoc()) {
                 $partitions[] = new partition($row["id"],  $row["password"], $row["id_user"], $row["web_address"], $row["description"], $row["login"]);
             }
-            return $partitions; //oddaje listę zakładek.
+            return $partitions; // return the array of partition.
 
         } else {
             return null;
         }
     }
 
+// Function that removes a record from the "password" table with the given id. Item id.
     public  function  deletePosition($id){
-
+// Query removing the item from the portfolio
         $sql= "DELETE from password where id='$id;'";
 
+// SQL execution
         if ($this->mysqli->query($sql) === TRUE) {
             return "Position removed"."\n";
         } else {
             return "Position was not removed". $this->mysqli->error;
         }
-
     }
 
     public  function decryptPassword($id){
-
+// query for a password with a given id
         $sql= "select password from password where id='$id'";
 
-      //  $ivlen = openssl_cipher_iv_length(constant('cypherMethod')); //Pobiera długość wektora inicjalizacji szyfru (iv).
-       // $iv = openssl_random_pseudo_bytes($ivlen); //Generuje ciąg pseudolosowych bajtów, z liczbą bajtów określoną przez lengthparametr.
-
-
-
+// query result
         $result = $this->mysqli->query($sql);
 
+// get the password from the database
         if ($result->num_rows > 0) {
-            // output data of each row
             while($row = $result->fetch_assoc()) {
                $password= $row["password"];
             }
         } else {
-            return null;
+            return null;// in case there is no such record
         }
-        $key = $_SESSION['password'];
+// decrypt the password from the "password" table using the encryption method from the config file and the user's password. 0 stands for OPENSSL_ZERO_PADDING
+        $decrypted = @openssl_decrypt($password, constant('cypherMethod'), $_SESSION['password'], 0);
 
-        $decrypted = @openssl_decrypt($password, constant('cypherMethod'), $key, $options=0);
-
+// Returns the decrypted password
         return $decrypted;
     }
 
-
-    /* funkcja select
-    public  function select($sql){
-
-        $result = $this->mysqli->query($sql);
-
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                echo print_r($row);       // Print the entire row data
-            }
-        } else {
-            echo "There is no results";
-        }
-
-    }
-    */
-
+// Function responsible for user login. Arguments: user login, password and the way in which his password was hashed.
     public  function login($login, $password, $keptAsHash){
-
+// create a user with a given login
         $user=$this->getUser($login);
 
-        if($user!=null){
-            if($keptAsHash==1)
+        if($user!=null){// if the user with the given login exists:
+            if($keptAsHash==1)// if encoded with SHA512
             {
-               @$loginPasswordHas = $this->hashPassword($password.$user->getSalt().constant('pepper')); //dołączenie stałej pepper z pliku config.
-
+               @$loginPasswordHas = $this->hashPassword($password.$user->getSalt().constant('pepper')); // call SHA512 with password from the form and combined salt and pepper from config.
             }
-            else{
-                @$loginPasswordHas = $this->HMACPassword($password,$user->getSalt().constant('pepper'));
-                //  $passwordHash = $this->HMACPassword($password.$salt.BD::pepper); //pepper jako stała, która wcześniej była zadeklarowana jako //const pepper = "6883589765f7e08e226dff0.01019947";.
+            else{//jeżeli jest kodowane MHAC
+                @$loginPasswordHas = $this->HMACPassword($password,$user->getSalt().constant('pepper')); // call the HMAC function with the password from the form and the salt and pepper combined
             }
 
             //sprawdzenie hashu
@@ -148,100 +129,78 @@ class BD {
                 $_SESSION['info'] = "You are now logged in";
                 $_SESSION['password'] = $password;
 
-                header('location: password_wallet.php'); //przeniesienie na odpowiednią stronę
-
-
+                header('location: password_wallet.php'); // transfer to the appropriate page
             }else{
                 return "Wrong password or hash type";
             }
-        }else{
+        }else{ // if there is no such user
             return "There is no user with that login!";
         }
     }
 
-    public  function registerUser($login, $password, $keptAsHash, $register){ //funkcja umożliwiająca tworzenie/edycję użytkownika
-
-        //tworzenie soli
-        $salt = substr(uniqid(mt_rand(), false), 0, 20); //utworzenie w funkcji uniqid ciągu 23 znaków, a następnie odcięcie 3 ostatnich (w bazie maksymalna długość soli to 20.
-        /*
-            //dodatkowe opcje do hashowania
-            $options = [
-                'salt' => $salt
-            ];
-            $passwordHash = password_hash("rasmuslerdorf", PASSWORD_BCRYPT, $options);
-             $hashPassword = $this->hashPassword($password);
-        */
-  //      echo "<hr>".$password.$salt.BD::pepper;
-
-       // echo @constant(pepper);  //@ukrycie warningów
+// function for creating a new user and changing the password
+    public  function registerUser($login, $password, $keptAsHash, $register){
+        // salt generating
+        $salt = substr(uniqid(mt_rand(), false), 0, 20); // create a string of 23 characters in the uniqid function, and then cut off the last 3, because the maximum salt length in the database is 20.
 
         if($keptAsHash)
-        { //jeżeli jest kodowane SHA512
-            @$passwordHash = $this->hashPassword($password.$salt.constant('pepper')); //dołączenie stałej pepper z pliku config.
+        { // if it is hashing with SHA512
+            @$passwordHash = $this->hashPassword($password.$salt.constant('pepper')); // include the constant pepper from config.
+            $keptAsHash=1;
         }
-        else{ //jeżeli jest kodowane MHAC
-            @$passwordHash = $this->HMACPassword($password, $salt.constant('pepper')); //polaczenie soli i pieprzu do funkcji hash_hmac
-            //  $passwordHash = $this->HMACPassword($password.$salt.BD::pepper); //pepper jako stała, która wcześniej była zadeklarowana jako //const pepper = "6883589765f7e08e226dff0.01019947";.
+        else{ // if it is hashing with HMAC
+            @$passwordHash = $this->HMACPassword($password, $salt.constant('pepper'));// combine salt and pepper for hash_hmac as key
+            $keptAsHash=0;
         }
+
+// saving the user's login and password in the session variables
         $_SESSION['login'] = $login;
         $_SESSION['password'] = $password;
 
-        //wywołanie funkcji wprowadzającej do bazy
+// call the introductory function to the database
         if($register){
             $insert_sql = 'INSERT INTO `user` (`id`, `login`, `password_hash`, `salt`, `isPasswordKeptAsHash`) VALUES (NULL, "'.$login.'", "'.$passwordHash.'", "'.$salt.'", "'.$keptAsHash.'");';
-            $this->insert($insert_sql);
-
-            $_SESSION['info'] = "You are now logged in";
-            header('location: password_wallet.php'); //przeniesienie na odpowiednią stronę
+            $this->insert($insert_sql); // call the function that introduces data to the database
+            $_SESSION['info'] = "You are now logged in"; // save the session variable with feedback
+            header('location: password_wallet.php'); // transfer to the appropriate page
         }else{
-            $update_sql = 'UPDATE USER SET `password_hash` = "'.$passwordHash.'", `salt` = "'.$salt.'", `isPasswordKeptAsHash` = "'.$keptAsHash.'" WHERE CustomerID = "'.$login.'");';
-            $this->insert($update_sql);
-
-            $_SESSION['info'] = "Your password has changed";
+            $update_sql = 'UPDATE user SET `password_hash`= "'.$passwordHash.'", `salt` = "'.$salt.'", `isPasswordKeptAsHash` = "'.$keptAsHash.'" WHERE login = "'.$login.'";';
+            $this->insert($update_sql); // call the insert function to the database
+            $_SESSION['info'] = "Your password has changed";// save the session variable with feedback
         }
     }
-/*     inna opcja hashu
-       public  function hashPassword($password){
-       $hashPassword =  password_hash($password, PASSWORD_DEFAULT);
-       return $hashPassword;
-    }
-    */
+
+// Function generating the hash value using the algorithm: SHA512. Arguments: password
     public  function hashPassword($password){
         $hashPassword = hash('sha512', $password);
-       // echo "<hr> hasło zahaszowane 2: ".$hashPassword;
-
         return $hashPassword;
     }
 
-    public  function HMACPassword($password, $salt){ //polaczenie soli i pieprzu do funkcji hash_hmac
-
-        /* dokumentacja
-            1.algo
+// A function that generates a hash value with a key, using the HMAC method. Arguments: password and salt
+    public  function HMACPassword($password, $salt){
+        /* hash_hmac
+            1.
             Name of selected hashing algorithm (i.e. "md5", "sha256", "haval160,4", etc..) See hash_hmac_algos() for a list of supported algorithms.
 
-            2.data
+            2.
             Message to be hashed.
 
             3.key
             Shared secret key used for generating the HMAC variant of the message digest.
-
-            4.raw_output
-            When set to TRUE, outputs raw binary data. FALSE outputs lowercase hexits.
          */
-
         $hashPassword =  hash_hmac('sha512', $password, $salt); //
         return $hashPassword;
     }
 
-    public  function resetPassword(){
+// Database introductory function - the argument is a SQL string
+    public function insert($sql) {
+        $feedback = "Something gone wrong";
 
-    }
-
-    public function insert($sql) { //funkcja insert
-        if(mysqli_real_escape_string($this->mysqli,$sql)) //Ta funkcja służy do tworzenia dozwolonego ciągu SQL, którego można używać w instrukcji SQL. Podany ciąg jest kodowany jako łańcuch znaków SQL ze zmianą znaczenia, biorąc pod uwagę bieżący zestaw znaków połączenia.
-        {
+        //mysqli_real_escape_string - function to create a allowed SQL string that can be used in a query.
+// The specified string is encoded as an SQL string.
+        if(mysqli_real_escape_string($this->mysqli,$sql)){
             if(mysqli_query($this->mysqli, $sql)==True){
-                $feedback= "<span>Rekordy został dodany poprawnie</span>";     //pokazuje, czy dodałoo niby
+                $feedback= "<span>Rekordy został dodany poprawnie</span>";
             }
             else {
                 $feedback= "<span>Błąd nie udało się dodać nowego rekordu</span>";
@@ -249,21 +208,5 @@ class BD {
         }
         return $feedback;
     }
-
-    public function getRows($sql) { //funkcja pobierająca dane
-        return mysqli_query($this->mysqli, $sql);
-    }
-
-    /*
-    public function delete() //funkcja usuwająca
-    {
-        $sql = "DELETE FROM user";
-        if ($this->mysqli->query($sql) === TRUE) {
-            echo "Poprzednie rekordy zostały usunięte prawidłowo."."\n";
-        } else {
-            echo "Wystąpił błąd podczas usuwania rekordów: " . $this->mysqli->error;
-        }
-    }
-    */
-};
+}
 ?>
