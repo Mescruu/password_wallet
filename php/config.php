ffc75c75aca5b019f6e9c929626528fc1e43bdc6
Mescruu <mescruu@gmail.com>
2020-10-12 22:04:06 +0200

    file with constants and database create function.

--- php/config.php
+++ php/config.php
@@ -1,12 +1,14 @@
 <?php
-
+//file with constants and db create function.
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'password_wallet');
-$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
-$db = new BD("localhost", "root", "", "password_wallet");
 
-define('pepper', '6883589765f7e08e226dff0x01019947');
-define('cypherMethod', 'AES-256-CBC');
+define('pepper', '6883589765f7e08e226dff0x01019947'); //pepper value
+define('cypherMethod', 'AES-256-CBC'); //cipher method
+
+function create_DB(){ //function allows to create database handler
+    return $db = new BD(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+}
